ef7bad33a06c432a676223d4fc8c88a4846d0dbb
Brandon Robbins <Brandon@Brandons-Mac-mini.local>
2014-10-02 23:51:18 -0700

    First Rev Of Project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -8,8 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var lblNameLabel: UILabel!
+    @IBOutlet weak var lblMessageLabel: UILabel!
 
+    @IBOutlet weak var txtNameTextBox: UITextField!
+    @IBOutlet weak var txtMessageTextBox: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +25,11 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func btnSendMessage(sender: UIButton)
+    {
     }
 
 
